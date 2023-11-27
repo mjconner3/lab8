ef8aea87cb3b3d0ce229eba3cf5c2b29a4b3fa0f
mjconner3 <143337576+mjconner3@users.noreply.github.com>
2023-11-27 10:45:43 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 function init(){
-//add your javascrip between these two lines of code
+/function init(){
+  var button = document.getElementById('entrybutton');
+
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Titus Lowe:This is a Test" + textbox.value);
+  }
+
+  button.addEventListener('click', showMeText);
+  }
+  
+window.addEventListener('load', init);/add your javascrip between these two lines of code
  
 
 
